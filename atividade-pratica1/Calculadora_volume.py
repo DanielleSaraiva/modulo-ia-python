6509b0e9150cf49818c2766d8696ddfa51e43f84
DanielleSaraiva <daniellerh25@gmail.com>
2025-10-02 20:33:09 -0300

    Entrada dinâmica com a função input e f-string

--- atividade-pratica1/Calculadora_volume.py
+++ atividade-pratica1/Calculadora_volume.py
@@ -1,12 +1,12 @@
 # Este programa calcula o volume de uma caixa retangular
 
 # Solicita as dimenções da caixa ao usuário
-comprimento = 12
-largura = 14
-altura = 20
+comprimento = int(input("Digite o comprimento da caixa: "))
+largura = int(input("Digite a largura da caixa: "))
+altura = int(input("Digite a altura da caixa: "))
 
 # calculo das dimenções
 volume = comprimento * largura * altura
 
 # Exibe o resultado
-print(volume)
+print(f"O volume da caixa é {volume} cm³")
